790b833d3b7c488e48d2d37cc950dd9c69cd63eb
mahesh <mahesh@digicollect.com>
2024-03-09 15:08:59 +0530

    completed header & footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import AllRoutes from "./routes/AllRoutes";
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<AllRoutes />
+			<Footer />
 		</div>
 	);
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const MovieList = () => {
-  return (
-    <main>MovieList</main>
-  )
+	return (
+		<main>
+			<div className="text-4xl text-red-600 font-bold">
+				MovieList
+			</div>
+		</main>
+	)
 }
 
 export default MovieList
